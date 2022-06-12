464a09baa1c2a0cc92d0d30d58a7a7285383f64a
MicaelD <micaeldiniz6@gmail.com>
2022-06-12 20:53:17 -0300

    Feat: documentation with swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,18 @@
 import { Express } from "express";
 import courseRouter from "./course.route";
 import userRouter from "./user.route";
+import swaggerUiExpress from "swagger-ui-express";
+import swaggerDocument from "./../../swagger.json"
 
 const registerRouters = (app: Express): void => {
 
     app.use(userRouter);
     app.use(courseRouter);
+    app.use(
+      "/api-documentation",
+      swaggerUiExpress.serve,
+      swaggerUiExpress.setup(swaggerDocument)
+    );
   };
   
 export default registerRouters;
